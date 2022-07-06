333a663a2d0fd49297567672145bacffb6ad9a8f
SohamMukherjee <sohammukherjee1996@gmail.com>
2022-07-07 00:18:34 +0530

    LL Implementation

--- LinkedList.java
+++ LinkedList.java
@@ -1,6 +1,7 @@
 public class LinkedList {
-
     public static Node head;
+    public static int sizeOfLL = 0;
+
     public static class Node{
         //Linked List Node Class
         String data;
@@ -11,7 +12,50 @@ public class LinkedList {
             next = null;
         }
     }
+    public static void deleteStart(){
+        //If No nodes return
+        if(head == null){
+            System.out.println("No nodes in the list");
+            return;
+        }
+        System.out.println("Decreasing size of LL");
+        sizeOfLL--;
+        head = head.next;
+        return;
 
+    }
+
+    public static void deleteLast(){
+        if(head == null){
+            System.out.println("No Nodes are Present");
+            return;
+        }
+
+        System.out.println("Decreasing size of LL");
+        sizeOfLL--;
+
+        if(head.next == null){
+            System.out.println("Only One Node Present That is Head, Making it null");
+            head = null;
+        }
+
+        Node secondLastNode = head;
+        Node lastNode = head.next;
+
+        System.out.println("Moving To the Second Last node");
+        while (lastNode.next != null){
+            lastNode = lastNode.next;
+            secondLastNode = secondLastNode.next;
+        }
+
+        System.out.println("Reached Last Node to Delete "+lastNode.data);
+
+        System.out.println("Disconnecting Last Node From the LL");
+        secondLastNode.next = null;
+        System.out.println("Deleted Last Node Successfully, now secondLastNode is pointing to null");
+
+
+    }
     //add Node to Beginning
     public static void addFirst(String data){
         //Creating new node to attach in list
@@ -20,9 +64,13 @@ public class LinkedList {
         //If this is the first Node then Set this to head and Return
         if(head == null){
             System.out.println("First Node in List Making this the head ");
+            //Increasing Size of LL
+            sizeOfLL++;
             head = newNode;
             return;
         }
+        System.out.println("Increasing size of LL");
+        sizeOfLL++;
         //Making Next Of New Node to Next
         newNode.next = head;
         //Making New Node the head as we are inserting in start
@@ -40,6 +88,7 @@ public class LinkedList {
             System.out.print(currentNode.data+"->");
             currentNode = currentNode.next;
         }
+        System.out.println("null");
     }
     public static void addEnd(String data){
         Node newNode = new Node(data);
@@ -47,10 +96,14 @@ public class LinkedList {
         //Checking if this is the first node
         if(head == null){
             System.out.println("This is the first node making it the head node");
+            //Increasing Size of LL
+            sizeOfLL++;
             head = newNode;
             return;
         }
 
+        System.out.println("Increasing Size of the List");
+        sizeOfLL++;
         //If This is not the First Node we need to Traverse to End and Attach it at the end
         Node currentNode = head;
         while (currentNode.next != null){
@@ -62,6 +115,9 @@ public class LinkedList {
         return;
     }
 
+    public static void printSizeOfLL(){
+        System.out.println("Size of Linked List is: "+sizeOfLL);
+    }
     public static void main(String[] args) {
         //Inserting at the Beginning
 //        addFirst("A1");
@@ -73,6 +129,13 @@ public class LinkedList {
         addEnd("A2");
         addEnd("A3");
         printLL(head);
+        printSizeOfLL();
+
+        //Deletion at start and end of the LL
+        deleteStart();
+        deleteLast();
+        printLL(head);
+        printSizeOfLL();
 
 
 
