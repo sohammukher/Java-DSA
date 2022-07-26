cfc9c461a199a47d77a29a9a815fc4f15004ad6d
SohamMukherjee <sohammukherjee1996@gmail.com>
2022-07-27 00:23:11 +0530

    Stack Using Collection

---
+++ StackUsingCollections.java
@@ -0,0 +1,11 @@
+import java.util.Stack;
+
+public class StackUsingCollections {
+    public static void main(String[] args) {
+        Stack stack = new Stack();
+
+        stack.push(1);
+
+        stack.pop();
+    }
+}
